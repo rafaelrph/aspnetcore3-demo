004250e72e49f4ca8dca76162c066a429ede8e94
Rafael Barros <rafaelbarros@Rafaels-MacBook-Air.local>
2020-05-08 15:12:15 -0700

    Added component

---
+++ Globomantics/Globomantics/ViewComponents/StatisticsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Globomantics.ViewComponents
+{
+    public class StatisticsViewComponent : ViewComponent
+    {
+        private readonly IConferenceService service;
+
+        public StatisticsViewComponent(IConferenceService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string statsCaption)
+        {
+            ViewBag.StatsCaption = statsCaption;
+            return View(await service.GetStatistics());
+        }
+    }
+}
