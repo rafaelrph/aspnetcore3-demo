c417a0e51b98280ad161d24de42072ea36f86e36
Rafael Barros <rafaelbarros@Rafaels-MacBook-Air.local>
2020-05-12 14:30:02 -0700

    Added API project

---
+++ Globomantics/API/Controllers/ConferenceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Shared.Services;
+using Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class ConferenceController : ControllerBase
+    {
+
+        private readonly IConferenceService service;
+
+        public ConferenceController(IConferenceService service)
+        {
+            this.service = service;
+        }
+
+        public ActionResult GetAll()
+        {
+            return Ok(service.GetAll());
+        }
+    }
+}
